f369fc79195d4885f39292e8fa72a81250bf6b69
Amr Fawaz <amr.abdelhakim.fawaz+75@gmail.com>
2024-07-04 02:12:03 +0300

    Initial Commit

---
+++ banquemisr.challenge05/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  banquemisr.challenge05
+//
+//  Created by AmrFawaz on 04/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ banquemisr.challenge05/banquemisr_challenge05App.swift
@@ -0,0 +1,17 @@
+//
+//  banquemisr_challenge05App.swift
+//  banquemisr.challenge05
+//
+//  Created by AmrFawaz on 04/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct banquemisr_challenge05App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
